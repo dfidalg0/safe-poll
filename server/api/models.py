115232d0bef2638a9e5510cc92ba090c2d85f4d4
Diego <diego-fidalgo@hotmail.com>
2020-11-05 19:55:39 -0300

    Adição dos modelos base

--- server/api/models.py
+++ server/api/models.py
@@ -1,25 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager
+)
 
 
 class UserAccountManager(BaseUserManager):
 
-    def base_create_user(self, email, name, password = None):
+    def base_create_user(self, email, name, password=None):
         if not email:
             raise ValueError('Usuário deve inserir um e-mail')
 
         email = self.normalize_email(email)
-        user = self.model(email = email, name = name)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         return user
 
-    def create_user(self, email, name, password = None):
+    def create_user(self, email, name, password=None):
         user = self.base_create_user(email, name, password)
         user.save()
         return user
 
-    def create_superuser(self, email, name, password = None):
+    def create_superuser(self, email, name, password=None):
         user = self.base_create_user(email, name, password)
         user.is_staff = True
         user.is_superuser = True
@@ -27,11 +30,11 @@ class UserAccountManager(BaseUserManager):
         return user
 
 
-class UserAccount(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length = 255, unique=True)
-    name = models.CharField(max_length = 255)
-    is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+class UserAccount(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserAccountManager()
     USERNAME_FIELD = 'email'
@@ -45,3 +48,69 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Group (models.Model):
+    name = models.CharField(max_length=100)
+    admin = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+    users = models.ManyToManyField(
+        UserAccount,
+        blank=True, related_name='groups'
+    )
+
+
+class PollType (models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Poll (models.Model):
+    # Informações básicas
+    name = models.CharField(max_length=100)
+    type = models.ForeignKey(PollType, on_delete=models.CASCADE)
+    description = models.TextField(null=True)
+
+    # Último dia para votação
+    deadline = models.DateField()
+
+    # Secretude dos votos
+    secret_vote = models.BooleanField()
+
+    # Administrador da eleição
+    admin = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
+
+    # Número de ganhadores da eleição
+    winners_number = models.PositiveIntegerField(default=1)
+
+    # Grupo de usuários associados
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, null=True)
+
+    # Soma máxima dos rankings (para os modelos Instant-runoff e Cumulative)
+    rankings_sum = models.PositiveIntegerField(null=True)
+
+    # Relação dos usuários que votaram
+    users_voted = models.ManyToManyField(
+        UserAccount,
+        blank=True, related_name='polls_voted'
+    )
+
+
+class Vote (models.Model):
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE)
+    option = models.ForeignKey('Option', on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        UserAccount,
+        null=True, on_delete=models.SET_NULL,
+        related_name='votes'
+    )
+    ranking = models.PositiveIntegerField()
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['poll', 'user'])
+        ]
+
+
+class Option (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
--- server/safepoll/urls.py
+++ server/safepoll/urls.py
@@ -25,4 +25,3 @@ urlpatterns = [
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
-
