394be2d60f774466611f0b95249d50a9a234b869
ArturAssisComp <arturassiscomp@gmail.com>
2020-11-05 19:53:06 -0300

    SAF-39 Adicionar chaves estrangeiras à tabela Token

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Token
-
+from .models import UserAccount, Token
 # Register your models here.
+admin.site.register(UserAccount),
 admin.site.register(Token)
--- server/api/models.py
+++ server/api/models.py
@@ -45,16 +45,15 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
-
 # Token.
 class TokenManager(models.Manager):
     #To create a new token, use: "new_token = Token.objects.create_token(email)"
-    def create_token(self, user_id, poll_id):
+    def create_token(self, user, poll):
         update_token_value = True
         token_value = self.generate_token()
         while len(self.get_queryset().filter(token=token_value)) != 0: #Loop condition: token_value already exists.
             token_value = self.generate_token()
-        new_token = self.create(token=token_value, user_id=user_id, poll_id = poll_id)
+        new_token = self.create(token=token_value, user=user, poll=poll)
         return new_token
 
     def generate_token(self):
@@ -68,6 +67,9 @@ class Token(models.Model):
         #return "Poll's ID (FK): {{insert poll's id here}}, e-mail: {0}".format(self.email)
 
     token   = models.CharField(max_length=500, unique=True)
-    poll_id = models.CharField(max_length=100)#models.ForeignKey
-    user_id = models.CharField(max_length=100)#models.ForeignKey
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE) 
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
     objects = TokenManager()
+
+class Poll(models.Model):
+    pass
