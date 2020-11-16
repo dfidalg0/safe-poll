7b7de20385836d06c2235001520b6d909831db8e
Ana Paula Schuch <aplschuch@gmail.com>
2020-11-16 02:45:14 -0300

    Modelo para views que precisam de autenticação

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,6 +1,9 @@
-from .views import HelloWorld
+from .views import *
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('hello', HelloWorld)
+    path('hello', HelloView.as_view()),
 ]
+
+
--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,15 @@
 from django.http import JsonResponse
 from random import randint
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from random import randint
 
 # Create your views here.
 
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
 
-def HelloWorld(req):
-    return JsonResponse({
-        'message': f'Hello, {randint(0, 100)}!'
-    })
+    def get(self, request):
+        content = {'message': f'Hello, {randint(0, 100)}!'}
+        return Response(content)
