69f5c30ab420c5b91bcefccbbb934dafb337181e
Diego <diego-fidalgo@hotmail.com>
2020-11-19 19:16:19 -0300

    Mudança de padrões de autenticação

--- server/safepoll/settings.py
+++ server/safepoll/settings.py
@@ -154,9 +154,6 @@ DJOSER = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
-    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
