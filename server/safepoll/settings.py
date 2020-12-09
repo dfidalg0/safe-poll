cf4d2bed1800d9a46ba80cedbfa3c154f4f16d33
Diego <diego-fidalgo@hotmail.com>
2020-12-09 20:04:17 -0300

    AGORA SIM VAI! FIZ TUDO... TEM QUE IR

--- server/safepoll/settings.py
+++ server/safepoll/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#!mjy-2_(a)4-hb&lptq&bg@ua3cps^69krg=7+&c8jppc@6-p'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '#!mjy-2_(a)4-hb&lptq&bg@ua3cps^69krg=7+&c8jppc@6-p')
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
