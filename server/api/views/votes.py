207875e26c859191645b1d1952cb90a6a17dd718
Ana Paula Schuch <aplschuch@gmail.com>
2021-05-08 23:17:54 -0300

    Implements backend for votes details

--- server/api/views/votes.py
+++ server/api/views/votes.py
@@ -50,3 +50,56 @@ def compute_vote(request: CleanRequest) -> Response:
 
     conclusion = {'vote_id': vote.id}
     return Response(conclusion)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+@with_rules({
+    'page': is_unsigned_int,
+    'per_page': is_unsigned_int,
+    'option_filter': lambda v: type(v) == str or v == None,
+    'email_filter': lambda v: type(v) == str or v == None,
+    'order_by': lambda v: type(v) == str or v == None
+})
+def get_poll_votes(request: CleanRequest, pk: int) -> Response:
+    try:
+        admin = request.user
+        poll = Poll.objects.get(pk=pk, admin=admin)
+
+    except Poll.DoesNotExist:
+        return Response({
+            'message': 'Eleição não encontrada'
+        }, status=HTTP_404_NOT_FOUND)
+    
+    votes_all = Vote.objects.filter(poll=poll)
+
+    data = request.clean_data
+    page = data['page']
+    per_page = data['per_page']
+
+    if 'email_filter' in data:
+        users_filter = UserAccount.objects.filter(ref__startswith = data['email_filter'])
+        votes_all = votes_all.filter(voter__in = users_filter)
+
+
+    if 'option_filter' in data:
+        votes_all = votes_all.filter(option__name__startswith = data['option_filter'])
+    
+    if 'order_by' in data:
+        order = data['order_by']
+        if order == 'option':
+            votes_all = votes_all.order_by('option__name')
+        elif order == '-option':
+            votes_all = votes_all.order_by('-option__name')
+        elif order == 'email':
+            votes_all = votes_all.order_by('voter__ref')
+        elif order == '-email':
+            votes_all = votes_all.order_by('-voter__ref')
+
+    votes = list(map(lambda vote: {
+        'option': vote.option.name,
+        'voter': vote.voter.ref
+    },
+        votes_all[(page-1)*per_page:per_page]
+    ))    
+    return Response(votes)
